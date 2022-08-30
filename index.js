e8e03a4fa8f4034b1812ae0501fa8909d271803b
Matin <matinjm7247@gmail.com>
2022-08-30 19:43:24 +0430

    feat : push all changes

--- index.js
+++ index.js
@@ -1,72 +1,79 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk'
-import inquirer from 'inquirer'
-import gradient from 'gradient-string'
-import chalkAnimation from 'chalk-animation'
-import figlet from 'figlet'
-import {createSpinner} from 'nanospinner'
-import * as fs from 'fs'
-import {dirname} from 'path'
-import {fileURLToPath} from 'url'
-import createDirectoryProject from './createDirectoryProject.js'
-const CURR_DIR = process.cwd()
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+import * as fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import createDirectoryProject from "./createDirectoryProject.js";
+const CURR_DIR = process.cwd();
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const CHOICES = fs.readdirSync(`${__dirname}/templates`)
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms)) 
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 async function Welcome() {
-    const projectNameRainbow = chalkAnimation.rainbow(
-        'Welcome to Nestjs Project Generator \n'
-    )
+  const projectNameRainbow = chalkAnimation.rainbow(
+    "Welcome to Nestjs Project Generator \n"
+  );
 
-    await sleep()
-    projectNameRainbow.stop()
+  await sleep();
+  projectNameRainbow.stop();
 }
 
-await Welcome()
+await Welcome();
 
 const QUESTIONS = [
-    {
-        name: 'project-choices',
-        type: 'list',
-        message: 'What project template would you like to generate?',
-        choices: CHOICES
+  {
+    name: "project-choices",
+    type: "list",
+    message: "What project template would you like to generate?",
+    choices: CHOICES,
+  },
+  {
+    name: "project-name",
+    type: "input",
+    message: "Project name :",
+    validate: function (input) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else
+        return "Project name may only include letters, numbers, underscores and hashed.";
     },
-    {
-        name: 'project-name',
-        type: 'input',
-        message: 'Project name :',
-        validate: function (input) {
-            if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
-            else 
-                return 'Project name may only include letters, numbers, underscores and hashed.';
-        }
-    }
-]
+  },
+];
 
-inquirer.prompt(QUESTIONS).then( answers => {
-    const projectChoices = answers['project-choices'],
-        projectName = answers['project-name'],
-        templatePath = `${__dirname}/templates/${projectChoices}`;
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectChoices = answers["project-choices"],
+    projectName = answers["project-name"],
+    templatePath = `${__dirname}/templates/${projectChoices}`;
 
-    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-    createDirectoryProject(templatePath,projectName)
+  fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+  createDirectoryProject(templatePath, projectName);
 
+  console.log(`
+        ${chalk.whiteBright(
+          `\r\ncd ${answers["project-name"]} \r\nnpm install \r\nnpm run start:dev`
+        )}
+    `); 
 
-    
-})
-
+  console.log(`
+        ${chalk.whiteBright(
+          `\r\ncd ${answers["project-name"]} \r\nnpm install \r\nnpm run start:dev`
+        )}
+    `);
+});
 
 // chalkAnimation.rainbow("hi Fuck")
 
-
 // let projectName ;
 
-// const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms)) 
+// const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms))
 
 // async function Welcome() {
 //     const projectNameRainbow = chalkAnimation.rainbow(
@@ -75,7 +82,6 @@ inquirer.prompt(QUESTIONS).then( answers => {
 
 //     await sleep()
 //     projectNameRainbow.stop()
-
 
 //     console.log(`
 //         ${chalk.bgBlueBright("Whats Your Project Name?")}
@@ -94,7 +100,7 @@ inquirer.prompt(QUESTIONS).then( answers => {
 //         }
 //     })
 //     projectName = answers['Project Name']
-    
+
 // }
 
 // await askProjectName()
